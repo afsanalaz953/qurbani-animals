e431b61bc894cab4e5407902d603eaf01312cc1c
afsanalaz953 <lazafsana7@gmail.com>
2026-05-02 02:09:56 +0600

    homepage finish

--- src/components/homepage/Feature.jsx
+++ src/components/homepage/Feature.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import Card from "@/components/ui/card"
 
-const FeatureAnimals = () => {
+const FeatureAnimals = async() => {
+const res = await fetch("https://qurbani-animals.vercel.app/data.json",{
+cache:"no-store"
+
+});
+const topAnimals = await res.json()
+   console.log(topAnimals, "topAnimals"); 
+   const topFeaturedAnimals = topAnimals.slice (0,4);
+   console.log(topFeaturedAnimals);
+   
+
+
+
     return (
-        <div>
-            <div className='bg-amber-100'>feature</div>
+        <div className='container mx-auto bg-slate-100 p-10'>
+            <div className =' text-3xl font-bold text-center m-10'>Top Featured Animals</div>
+              <div className='grid grid-cols-4'>
+                {topFeaturedAnimals.map ((featureAnimals, ind) => {
+                return <Card animals = {featureAnimals} key = {ind}/>
+              })}
+                
+                </div>
+           
         </div>
     );
 };
---
+++ src/components/ui/card.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from "next/link"
+import Image from "next/image"
+
+const Cards = ({animals}) => {
+    return (
+        <div className='grid grid-cols-4 gap-2 bg-slate-100 '>
+<div className="card bg-white shadow-sm w-max">
+  <figure className="px-10 pt-10 ">
+    <Image
+      src={animals.image}
+      alt="animals"
+      width={200}
+      height={50}
+      
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">{animals.name}</h2>
+    <p>Breed: {animals.breed}</p>
+    <p>Age: {animals.age}</p>
+    <p>Weight:{animals.weight}</p>
+    <p className='text-2xl font-bold text-green-500'>Price: {animals.price}</p>
+   
+    <div className="card-actions">
+      <button className="btn btn-neutral font-bold w-full"> <Link href = "/animals">View Details</Link></button>
+    </div>
+  </div>
+</div>
+    
+        </div>
+  );      
+     
+};
+
+export default Cards;
